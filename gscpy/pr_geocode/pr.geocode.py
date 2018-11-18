db9cb8059771b0edfb8aeddcccec30e959100c5e
ibaris <i.baris@outlook.de>
2018-11-18 18:44:21 +0100

    feat(gscpy.pr_geocode.pr.geocpde.py): Bind pyroSAR as preprocessor

--- gscpy/pr_geocode/pr.geocode.py
+++ gscpy/pr_geocode/pr.geocode.py
@@ -2,7 +2,7 @@
 
 ############################################################################
 #
-# MODULE:       pp.geocode
+# MODULE:       pr.geocode
 #
 # AUTHOR(S):    Ismail Baris
 #
@@ -16,166 +16,227 @@
 #
 #############################################################################
 
-# %module
-# % description: Wrapper function for geocoding SAR images using ESA SNAP.
-# % keyword: processing
-# % keyword: sar
-# % keyword: sentinel 1
-# %end
+#%module
+#% description: Wrapper function for geocoding SAR images using ESA SNAP.
+#% keyword: processing
+#% keyword: sar
+#% keyword: sentinel 1
+#%end
 
 # Input Section --------------------------------------------------------------------------------------------------------
-# %option G_OPT_F_INPUT
-# % key: infile
-# % multiple: no
-# % description: The SAR scene to be processed:
-# %guisection: Input
-# %end
+#%option G_OPT_F_INPUT
+#% key: infile
+#% multiple: no
+#% description: The SAR scene to be processed:
+#%guisection: Input
+#%end
 
-# %option G_OPT_F_INPUT
-# % key: shapefile
-# % required: no
-# % multiple: no
-# % description: A vector geometry for subsetting the SAR scene to a test site:
-# %guisection: Input
-# %end
+#%option G_OPT_F_INPUT
+#% key: shapefile
+#% required: no
+#% multiple: no
+#% description: A vector geometry for subsetting the SAR scene to a test site:
+#%guisection: Input
+#%end
 
-# %option
-# % key: EPSG
-# % type: int
-# % required: no
-# % multiple: no
-# % description: A target geographic reference system in EPSG, (Default is 4326):
-# %guisection: Input
-# %end
+#%option
+#% key: epsg
+#% type: int
+#% required: no
+#% multiple: no
+#% description: A target geographic reference system in EPSG, (Default is 4326):
+#%guisection: Input
+#%end
 
 # Output Section -------------------------------------------------------------------------------------------------------
-# %option
-# % key: resolution_value
-# % type: double
-# % required: no
-# % multiple: no
-# % description: Resolution of output raster map:
-# % guisection: Output
-# %end
+#%option G_OPT_F_INPUT
+#% key: outdir
+#% multiple: no
+#% description: The directory to write the final files to:
+#%guisection: Input
+#%end
 
-# %option
-# % key: scaling
-# % type: string
-# % required: no
-# % multiple: no
-# % answer: dB
-# % options: dB,linear
-# % description: Should the output be in linear or decibel scaling?:
-# % guisection: Output
-# %end
+#%option
+#% key: resolution_value
+#% type: double
+#% required: no
+#% multiple: no
+#% description: Resolution of output raster map:
+#% guisection: Output
+#%end
 
-# %option
-# % key: geocoding_type
-# % type: string
-# % required: no
-# % multiple: no
-# % answer: Range-Doppler
-# % options: Range-Doppler, Cross-Correlation
-# % description: The type of geocoding applied:
-# % guisection: Output
-# %end
+#%option
+#% key: scaling
+#% type: string
+#% required: no
+#% multiple: no
+#% answer: dB
+#% options: dB,linear
+#% description: Should the output be in linear or decibel scaling?:
+#% guisection: Output
+#%end
+
+#%option
+#% key: geocoding_type
+#% type: string
+#% required: no
+#% multiple: no
+#% answer: Range-Doppler
+#% options: Range-Doppler, Cross-Correlation
+#% description: The type of geocoding applied:
+#% guisection: Output
+#%end
 
 # Optional Section -----------------------------------------------------------------------------------------------------
-# %option
-# % key: polarizations
-# % type: string
-# % required: no
-# % multiple: yes
-# % description: The polarizations to be processed (Default is 'all'):
-# % guisection: Optional
-# %end
+#%option
+#% key: polarizations
+#% type: string
+#% required: no
+#% multiple: yes
+#% description: The polarizations to be processed (Default is 'all'):
+#% guisection: Optional
+#%end
 
-# %option
-# % key: removeS1BoderNoise
-# % type: string
-# % required: no
-# % multiple: no
-# % answer: True
-# % options: True, False
-# % description: Enables removal of S1 GRD border noise:
-# % guisection: Optional
-# %end
+#%option
+#% key: remove_boder_noise
+#% type: string
+#% required: no
+#% multiple: no
+#% answer: True
+#% options: True, False
+#% description: Enables removal of S1 GRD border noise:
+#% guisection: Optional
+#%end
 
-# %option
-# % key: offset
-# % type: integer
-# % required: no
-# % multiple: yes
-# % description: Integers defining offsets for left, right, top and bottom in pixels, e.g. 100, 100, 0, 0:
-# % guisection: Optional
-# %end
+#%option
+#% key: offset
+#% type: integer
+#% required: no
+#% multiple: yes
+#% description: Integers defining offsets for left, right, top and bottom in pixels, e.g. 100, 100, 0, 0:
+#% guisection: Optional
+#%end
 
-# %option G_OPT_F_INPUT
-# % key: externalDEMFile
-# % required: no
-# % multiple: no
-# % description: The absolute path to an external DEM file:
-# %guisection: Optional
-# %end
+#%option G_OPT_F_INPUT
+#% key: external_dem_file
+#% required: no
+#% multiple: no
+#% description: The absolute path to an external DEM file:
+#%guisection: Optional
+#%end
 
-# %option
-# % key: externalDEMNoDataValue
-# % type: integer
-# % required: no
-# % multiple: no
-# % description: The no data value of the external DEM:
-# % guisection: Optional
-# %end
+#%option
+#% key: external_dem_nan
+#% type: integer
+#% required: no
+#% multiple: no
+#% description: The no data value of the external DEM:
+#% guisection: Optional
+#%end
 
-# %option
-# % key: externalDEMApplyEGM
-# % type: string
-# % required: no
-# % multiple: no
-# % answer: True
-# % options: True, False
-# % description: Apply Earth Gravitational Model to external DEM?:
-# % guisection: Optional
-# %end
+#%option
+#% key: external_dem_egm
+#% type: string
+#% required: no
+#% multiple: no
+#% answer: True
+#% options: True, False
+#% description: Apply Earth Gravitational Model to external DEM?:
+#% guisection: Optional
+#%end
 
-# %option
-# % key: test
-# % type: string
-# % required: no
-# % multiple: no
-# % answer: True
-# % options: True, False
-# % description: If set to True the workflow xml file is only written and not executed:
-# % guisection: Optional
-# %end
+#%option
+#% key: test
+#% type: string
+#% required: no
+#% multiple: no
+#% answer: False
+#% options: True, False
+#% description: Write only the workflow in xml file:
+#% guisection: Optional
+#%end
 
 import sys
+import os
 from pyroSAR.snap.util import geocode
 import grass.script as gscript
 
 
 def main():
     options, flags = gscript.parser()
+
     infile = options['infile']
+    outdir = options['outdir']
     shapefile = options['shapefile']
-    t_srs = options['EPSG']
+    t_srs = options['epsg']
     tr = options['resolution_value']
     scaling = options['scaling']
     geocoding_type = options['geocoding_type']
     polarizations = options['polarizations']
-    removeS1BoderNoise = options['removeS1BoderNoise']
+    removeS1BoderNoise = options['remove_boder_noise']
     offset = options['offset']
-    externalDEMFile = options['externalDEMFile']
-    externalDEMNoDataValue = options['externalDEMNoDataValue']
-    externalDEMApplyEGM = options['externalDEMApplyEGM']
+    externalDEMFile = options['external_dem_file']
+    externalDEMNoDataValue = options['external_dem_nan']
+    externalDEMApplyEGM = options['external_dem_egm']
     test = options['test']
-    outdir = "/media/ibaris/Shared/Documents/GRASS_GIS_DB/GSCPY/PERMANENT"
 
-    print(options)
-    # geocode(infile, outdir, t_srs=t_srs, tr=tr, polarizations=polarizations, shapefile=shapefile, scaling=scaling,
-    #         geocoding_type=geocoding_type, removeS1BoderNoise=removeS1BoderNoise, offset=offset,
-    #         externalDEMFile=externalDEMFile, externalDEMNoDataValue=externalDEMNoDataValue,
-    #         externalDEMApplyEGM=externalDEMApplyEGM, basename_extensions=None, test=test)
+    if shapefile is '':
+        shapefile = None
+
+    if outdir is '':
+        outdir = os.path.dirname(infile)
+
+    if t_srs is '':
+        t_srs = 4326
+
+    if tr is '':
+        tr = 20
+
+    if geocoding_type is 'Cross-Correlation':
+        geocoding_type = 'SAR simulation cross correlation'
+
+    if polarizations is 'all':
+        pass
+    elif polarizations is '':
+        polarizations = 'all'
+    else:
+        polarizations = [polarizations]
+
+    if removeS1BoderNoise is 'True':
+        removeS1BoderNoise = True
+    else:
+        removeS1BoderNoise = False
+
+    if offset is '':
+        offset = None
+    else:
+        offset_list = offset.split(',')
+        offset_list = [int(item) for item in offset_list]
+        offset = tuple(offset_list)
+
+    if externalDEMFile is '':
+        externalDEMFile = None
+
+    if externalDEMNoDataValue is '':
+        externalDEMNoDataValue = None
+    else:
+        externalDEMNoDataValue = int(externalDEMNoDataValue)
+
+    if externalDEMApplyEGM is 'True':
+        externalDEMApplyEGM = True
+    else:
+        externalDEMApplyEGM = False
+
+    if test is 'True':
+        test = True
+    else:
+        test = False
+
+
+    geocode(infile, outdir, t_srs=t_srs, tr=tr, polarizations=polarizations, shapefile=shapefile, scaling=scaling,
+            geocoding_type=geocoding_type, removeS1BoderNoise=removeS1BoderNoise, offset=offset,
+            externalDEMFile=externalDEMFile, externalDEMNoDataValue=externalDEMNoDataValue,
+            externalDEMApplyEGM=externalDEMApplyEGM, test=test)
 
     return 0
 
