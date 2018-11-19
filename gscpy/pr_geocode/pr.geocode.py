3593a5a6f697c69742bb4a0e80cf2d9005fbbf6a
ibaris <i.baris@outlook.de>
2018-11-19 18:18:20 +0100

    feat(gscpy.pr_geocode.pr.geocode.py): Add a wrapper class to enable multiple processing.

--- gscpy/pr_geocode/pr.geocode.py
+++ gscpy/pr_geocode/pr.geocode.py
@@ -25,10 +25,10 @@
 #%end
 
 # Input Section --------------------------------------------------------------------------------------------------------
-#%option G_OPT_F_INPUT
-#% key: infile
+#%option G_OPT_M_DIR
+#% key: dir
 #% multiple: no
-#% description: The SAR scene to be processed:
+#% description: Name for input directory where Sentinel data lives:
 #%guisection: Input
 #%end
 
@@ -42,7 +42,6 @@
 
 #%option
 #% key: epsg
-#% type: int
 #% required: no
 #% multiple: no
 #% description: A target geographic reference system in EPSG, (Default is 4326):
@@ -87,6 +86,13 @@
 #% options: Range-Doppler, Cross-Correlation
 #% description: The type of geocoding applied:
 #% guisection: Output
+#%end
+
+# Filter Section -------------------------------------------------------------------------------------------------------
+#%option
+#% key: pattern
+#% description: File name pattern to import
+#% guisection: Filter
 #%end
 
 # Optional Section -----------------------------------------------------------------------------------------------------
@@ -157,19 +163,121 @@
 #% description: Write only the workflow in xml file:
 #% guisection: Optional
 #%end
+
+# Print Section --------------------------------------------------------------------------------------------------------
+#%flag
+#% key: p
+#% description: Print raster data to be imported and exit
+#% guisection: Print
+#%end
 """
 
 import os
 import sys
+import re
+import datetime as dt
 
-import grass.script as gscript
+import grass.script as gs
 from pyroSAR.snap.util import geocode
 
 
-def main():
-    options, flags = gscript.parser()
+class Geocode(object):
+    def __init__(self, dir, outdir, pattern=None, t_srs=4326, tr=20, polarizations='all', shapefile=None, scaling='dB',
+                 geocoding_type='Range-Doppler', removeS1BoderNoise=True, offset=None, externalDEMFile=None,
+                 externalDEMNoDataValue=None, externalDEMApplyEGM=True, basename_extensions=None, test=False,
+                 verbose=False):
 
-    infile = options['infile']
+        # Initialize DIRS ----------------------------------------------------------------------------------------------
+        self._dir_list = []
+
+        if not os.path.exists(dir):
+            gs.fatal(_('Input directory <{}> not exists').format(dir))
+        else:
+            self.dir = dir
+
+        if not os.path.exists(outdir):
+            os.makedirs(outdir)
+        else:
+            self.outdir = dir
+
+        # Create Pattern and find files --------------------------------------------------------------------------------
+        self.extension = '.zip'
+
+        if pattern:
+            filter_p = pattern + self.extension
+        else:
+            filter_p = r'.*.' + self.extension
+
+        self.filter_p = filter_p
+
+        gs.debug('Filter: {}'.format(filter_p), 1)
+        self.files = self.__filter(filter_p)
+
+        # Self definitions ---------------------------------------------------------------------------------------------
+
+        self.t_srs = t_srs
+        self.tr = tr
+        self.polarizations = polarizations
+        self.shapefile = shapefile
+        self.scaling = scaling
+        self.geocoding_type = geocoding_type
+        self.removeS1BoderNoise = removeS1BoderNoise
+        self.offset = offset
+        self.externalDEMFile = externalDEMFile
+        self.externalDEMNoDataValue = externalDEMNoDataValue
+        self.externalDEMApplyEGM = externalDEMApplyEGM
+        self.basename_extensions = basename_extensions
+        self.test = test
+        self.verbose = verbose
+
+    # ------------------------------------------------------------------------------------------------------------------
+    # Public Methods
+    # ------------------------------------------------------------------------------------------------------------------
+    def geocode(self):
+        for infile in self.files:
+            if self.verbose:
+                sys.stdout.write('Start Time: {0}'.format(dt.datetime.utcnow().__str__()))
+                sys.stdout.write('Start Processing File: {0} {1}'.format(str(os.path.basename(infile)), os.linesep))
+
+            geocode(infile, self.outdir, t_srs=self.t_srs, tr=self.tr, polarizations=self.polarizations,
+                    shapefile=self.shapefile, scaling=self.scaling, geocoding_type=self.geocoding_type,
+                    removeS1BoderNoise=self.removeS1BoderNoise, offset=self.offset,
+                    externalDEMFile=self.externalDEMFile, externalDEMNoDataValue=self.externalDEMNoDataValue,
+                    externalDEMApplyEGM=self.externalDEMApplyEGM, test=self.test)
+
+            if self.verbose:
+                sys.stdout.write('End Time: {0}'.format(dt.datetime.utcnow().__str__()))
+                sys.stdout.flush()
+
+    def print_products(self):
+        for f in self.files:
+            sys.stdout.write('{0} {1}'.format(str(f), os.linesep))
+
+    # ------------------------------------------------------------------------------------------------------------------
+    # Private Methods
+    # ------------------------------------------------------------------------------------------------------------------
+    def __filter(self, filter_p):
+        pattern = re.compile(filter_p)
+        files = []
+        for rec in os.walk(self.dir):
+            if not rec[-1]:
+                continue
+
+            match = filter(pattern.match, rec[-1])
+            if match is None:
+                continue
+
+            for f in match:
+                if f.endswith(self.extension):
+                    files.append(os.path.join(rec[0], f))
+
+        return files
+
+
+def main():
+    options, flags = gs.parser()
+
+    dir = options['dir']
     outdir = options['outdir']
     shapefile = options['shapefile']
     t_srs = options['epsg']
@@ -184,14 +292,21 @@ def main():
     externalDEMApplyEGM = options['external_dem_egm']
     test = options['test']
 
+    if options['pattern'] == '':
+        pattern = None
+    else:
+        pattern = options['pattern']
+
     if shapefile is '':
         shapefile = None
 
     if outdir is '':
-        outdir = os.path.dirname(infile)
+        outdir = os.path.join(os.path.dirname(dir), 'results')
 
     if t_srs is '':
         t_srs = 4326
+    else:
+        t_srs = int(t_srs)
 
     if tr is '':
         tr = 20
@@ -236,10 +351,18 @@ def main():
     else:
         test = False
 
-    geocode(infile, outdir, t_srs=t_srs, tr=tr, polarizations=polarizations, shapefile=shapefile, scaling=scaling,
-            geocoding_type=geocoding_type, removeS1BoderNoise=removeS1BoderNoise, offset=offset,
-            externalDEMFile=externalDEMFile, externalDEMNoDataValue=externalDEMNoDataValue,
-            externalDEMApplyEGM=externalDEMApplyEGM, test=test)
+    if flags['p']:
+        verbose = True
+    else:
+        verbose = False
+
+    pp_geocode = Geocode(dir=dir, outdir=outdir, pattern=pattern, t_srs=t_srs, tr=tr, polarizations=polarizations,
+                         shapefile=shapefile, scaling=scaling, geocoding_type=geocoding_type,
+                         removeS1BoderNoise=removeS1BoderNoise, offset=offset, externalDEMFile=externalDEMFile,
+                         externalDEMNoDataValue=externalDEMNoDataValue, externalDEMApplyEGM=externalDEMApplyEGM,
+                         test=test, verbose=verbose)
+
+    pp_geocode.geocode()
 
     return 0
 
